ce748dd963adc5874128445bd89db4df42a28af5
Brett Larson <larson.brett.s@gmail.com>
2025-02-11 08:33:01 -0500

    Update main.py
    Update logic to always get active and canceled accounts within one run.

--- src/get_managed_accounts/main.py
+++ src/get_managed_accounts/main.py
@@ -20,8 +20,13 @@ def main():
     # Load environment variables
     load_dotenv()
 
-    # Initialize account list result variable for use in the application
+    # Initialize variables
     account_list_result = None
+    get_active_account_variables = {"isCanceled": False}
+    get_canceled_account_variables = {"isCanceled": True}
+    csv_active_filename = 'active_accounts.csv'
+    csv_canceled_filename = 'canceled_accounts.csv'
+
 
     # Initialize handlers
     accounts_handler = NewRelicAccountsHandler(chunk_size=1000)
@@ -30,19 +35,33 @@ def main():
     # rate_limiter = RateLimiter(calls_per_minute=2000)
     # rate_limiter.wait_if_needed()
 
-    get_managed_account_variables = {"isCanceled": False}
 
     try:
-        logger.info("Executing GraphQL query to get accounts.")
-        account_list_result = execute_query(query=get, variables=get_managed_account_variables)
+        logger.info("Executing GraphQL query to get active accounts.")
+        account_list_result = execute_query(query=get, variables=get_active_account_variables)
     except Exception as e:
         logger.error(f"Error: {e}")
 
     try:
-        logger.info("Writing accounts to CSV.")
-        accounts_handler.write_accounts_to_csv('active_accounts.csv', account_list_result)
+        logger.info("Writing active accounts to CSV.")
+        accounts_handler.write_accounts_to_csv(csv_active_filename, account_list_result)
     except Exception as e:
         logger.error(f"Error: {e}")
+
+
+    try:
+        logger.info("Executing GraphQL query to get canceled accounts.")
+        account_list_result = execute_query(query=get, variables=get_canceled_account_variables)
+    except Exception as e:
+        logger.error(f"Error: {e}")
+
+    try:
+        logger.info("Writing canceled accounts to CSV.")
+        accounts_handler.write_accounts_to_csv(csv_canceled_filename, account_list_result)
+    except Exception as e:
+        logger.error(f"Error: {e}")
+
+    logger.info("Application finished.")
 
 if __name__ == "__main__":
     main()
